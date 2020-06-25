2af6140e2fe18e578701134baa2704ab74ee8a6c
Shrey Tiwari <shrey2697@gmail.com>
2020-06-25 14:08:01 +0530

    Setting up nuxt.js color mode

---
+++ components/ColorModePicker.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="button-div">
+    <div v-for="color in colors" :key="color">
+      <el-button @click="$colorMode.preference = color" class="button-nuxt">{{
+        color.toUpperCase()
+      }}</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      colors: ["system", "light", "dark", "sepia"]
+    };
+  }
+};
+</script>
+<style>
+.button-div {
+  display: flex;
+  justify-content: space-between;
+}
+.button-nuxt {
+  width: 200px;
+}
+</style>
